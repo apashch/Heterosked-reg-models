3e4901b49ef8296bd1a2e644d146933a145495b5
apashch <apashch@users.noreply.github.com>
2018-06-04 22:49:00 -0700

    Revisited WLSE, GLSEI, and GLSEII

--- GLSE.py
+++ GLSE.py
@@ -13,31 +13,27 @@ def LSE(data):
 		a1, a2 = lr.intercept_, lr.coef_[1]
 		return [a1, a2, lr.predict(X), y]
 
-def GLSE(data, error_dist = lambda x: np.log(abs(x))):
+def WLSE(data):
+	homdata = data.multiply(data.x.abs().pow(-0.5), axis = 0)
+	return LSE(homdata)[:-1] + [data.y]
+
+def GLSEI(data, error_dist = lambda x: np.log(abs(x))):
 	res2 = get_residuals(data)**2
 	aux_data = pd.DataFrame(data = {'y': np.log(res2), 'z': error_dist(data.x), 'const': [1]*len(data.x)})
 	a1, a2, yhat, y = LSE(aux_data)
 	sigmahat2 = np.exp(aux_data.z*a2 + a1)
-	homdata = data.multiply(sigmahat2.pow(-1), axis = 0)
-	print("GLSE coefficents:", a1, a2)
+	homdata = data.multiply(sigmahat2.pow(-0.5), axis = 0)
+	print("GLSEI coefficents:", a1, a2)
 	return LSE(homdata)[:-1] + [data.y]
 
-def WLSE_sq(data):
-	res2 = get_residuals(data)**2
-	aux_data = pd.DataFrame(data = {'y': res2,'x': data.x,'x2': data.x.pow(2),'const': [1]*len(data.x)})
-	a1, a2, a3, yhat, y = LSE(aux_data)
-	#print(a1, a2, a3)
-	sigmahat2 = aux_data.x2.pow(2)*a3+aux_data.x*a2 + a1
-	homdata = data.multiply(sigmahat2.pow(-1), axis = 0)
-	return LSE(homdata)[:-1] + [data.y]
-
-def WLSE_lin(data):
+def GLSEII(data):
 	res2 = get_residuals(data)**2
 	aux_data = pd.DataFrame(data = {'y': res2,'x': data.x,'const': [1]*len(data.x)})
 	a1, a2, yhat, y = LSE(aux_data)
-	#print(a1, a2)
-	sigmahat2 = aux_data.x*a2 + a1
-	homdata = data.multiply(sigmahat2.pow(-1), axis = 0)
+	sigmahat2 = aux_data.x.abs()*a2 + a1
+	sigmahat2[sigmahat2 < 0] = 1
+	homdata = data.multiply(sigmahat2.pow(-0.5), axis = 0)
+	print("GLSEII coefficents:", a1, a2)
 	return LSE(homdata)[:-1] + [data.y]
 
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils import *
-from GLSE import *
+from NONLIN import *
 from LASSO import *
 import matplotlib.pyplot as plt
 
@@ -8,13 +8,12 @@ if __name__ == "__main__":
 	df = getdata()
 	o = dict()
 	for dset in range(1, 6):
-		#print(heteroskedacity_test(df[dset]))
-		o[dset] = [LSE(df[dset]), GLSE(df[dset]), LASSO(df[dset]), Ridge(df[dset]), Huber(df[dset]), WLSE_sq(df[dset]), WLSE_lin(df[dset])]
+		o[dset] = [LSE(df[dset]), GLSEI(df[dset]), GLSEII(df[dset]), WLSE(df[dset]), LASSO(df[dset]), Ridge(df[dset]), Huber(df[dset])]
 	for k in o.keys():
 		print(k, end = ' ')
 		print(heteroskedacity_test(df[k]))
 		for m in range(len(o[1])):
-		 #print(*model_evaluation(o[k][m]))
+		 print(*model_evaluation(o[k][m]))
 		 continue
 
 
@@ -22,16 +21,16 @@ if __name__ == "__main__":
 		fig, ax = plt.subplots(figsize=(18, 12))
 		ax.scatter(df[k].x, df[k].y)
 		ax.plot(df[k].x, o[k][0][-2], color = 'b', label = 'LSE')
-		ax.plot(df[k].x, o[k][1][0] + o[k][1][1]*df[k].x, color = 'r', label = 'GLSE')
-		ax.plot(df[k].x, o[k][-1][0] + o[k][-1][1]*df[k].x, color = 'g', label = 'WLSE')
-		ax.plot(df[k].x, o[k][-2][0] + o[k][-2][1]*df[k].x, label = 'WLSE_sq')
+		ax.plot(df[k].x, o[k][1][0] + o[k][1][1]*df[k].x, color = 'r', label = 'GLSEI')
+		ax.plot(df[k].x, o[k][2][0] + o[k][2][1]*df[k].x, color = 'g', label = 'GLSEII')
+		ax.plot(df[k].x, o[k][3][0] + o[k][3][1]*df[k].x, label = 'WLSE')
 		legend = ax.legend()
 		plt.savefig("pred_plot_{}_new".format(k))
 		plt.close()
 
-	for k in o.keys():
-		fig, ax = plt.subplots(figsize=(18, 12))
-		ax.scatter(df[k].x, get_residuals2(df[k].x, o[k][1]))
-		plt.savefig("GLSE_residuals_{}".format(k))
-		plt.close()
+	# for k in o.keys():
+	# 	fig, ax = plt.subplots(figsize=(18, 12))
+	# 	ax.scatter(df[k].x, get_residuals2(df[k].x, o[k][1]))
+	# 	plt.savefig("GLSE_residuals_{}".format(k))
+	# 	plt.close()
 
