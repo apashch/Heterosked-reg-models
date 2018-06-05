a4244d52ae4172ce9900c93ef64c7a9b0102af98
apashch <apashch@users.noreply.github.com>
2018-06-04 23:10:38 -0700

    New color for WLSE on plots

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import *
+from GLSE import *
 from NONLIN import *
-from LASSO import *
 import matplotlib.pyplot as plt
 
 
@@ -9,12 +9,22 @@ if __name__ == "__main__":
 	o = dict()
 	for dset in range(1, 6):
 		o[dset] = [LSE(df[dset]), GLSEI(df[dset]), GLSEII(df[dset]), WLSE(df[dset]), LASSO(df[dset]), Ridge(df[dset]), Huber(df[dset])]
+	
+	#get R2 values in a table
 	for k in o.keys():
-		print(k, end = ' ')
-		print(heteroskedacity_test(df[k]))
+		#print(k)
 		for m in range(len(o[1])):
-		 print(*model_evaluation(o[k][m]))
-		 continue
+			print(model_evaluation(o[k][m])[0], end = ',')
+		print(' ')
+
+	#get RMSE values in a table
+	for k in o.keys():
+		#print(k)
+		for m in range(len(o[1])):
+			print(model_evaluation(o[k][m])[2], end = ',')
+		print(' ')
+
+			
 
 
 	for k in o.keys():
@@ -23,7 +33,7 @@ if __name__ == "__main__":
 		ax.plot(df[k].x, o[k][0][-2], color = 'b', label = 'LSE')
 		ax.plot(df[k].x, o[k][1][0] + o[k][1][1]*df[k].x, color = 'r', label = 'GLSEI')
 		ax.plot(df[k].x, o[k][2][0] + o[k][2][1]*df[k].x, color = 'g', label = 'GLSEII')
-		ax.plot(df[k].x, o[k][3][0] + o[k][3][1]*df[k].x, label = 'WLSE')
+		ax.plot(df[k].x, o[k][3][0] + o[k][3][1]*df[k].x, color = 'm', label = 'WLSE')
 		legend = ax.legend()
 		plt.savefig("pred_plot_{}_new".format(k))
 		plt.close()
